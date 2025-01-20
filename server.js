82a9e0612d4dd54e1ab213fd0542a3296362269c
Erik Bäckner <erikbackner@gmail.com>
2025-01-20 11:48:09 +0100

    added connect mongodb

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require ("express");
 const app = express();
-
+const mongoose = require ("mongoose");
 const PORT = 3000;
 
-app.listen(PORT, () =>{
-    console.log ("listening to 3000");
+app.get ("/", (req, res)=> {
+    res.send ("welcome to my mongoAPI");
 });
+
+app.post ("/blogpost", (req, res) =>{
+
+    res.send ("this is a postrequest");
+});
+
+mongoose.connect("mongodb+srv://erikbackner1:54321edith@eriksapi.9d1xz.mongodb.net/products-API?retryWrites=true&w=majority&appName=EriksAPI")
+    .then(() => {
+        console.log("connected to mongo");
+        app.listen(PORT, () =>{
+            console.log ("listening to 3000");
+        });
+    });
